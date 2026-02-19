77e6663eebc0fc4ae173dacc78b853b00df98144
felipy <felipy81br@gmail.com>
2026-02-19 14:52:06 +0000

    feat: create client entity with test in wallet-core

---
+++ wallet-core/internal/entity/client_test.go
@@ -0,0 +1,41 @@
+package entity
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCreateNewClient(t *testing.T) {
+	client, err := NewClient("John Doe", "j0j.com")
+	/*
+		esse if é a mesma coisa que assert.Nil(t, err)
+		if err != nil {
+			t.Errorf("Error creating new client: %v", err)
+		}
+	*/
+	assert.Nil(t, err)
+	assert.NotNil(t, client)
+	assert.Equal(t, "John Doe", client.Name)
+	assert.Equal(t, "j0j.com", client.Email)
+}
+
+func TestCreateNewClientWhenArgsAreInvalid(t *testing.T) {
+	client, err := NewClient("", "")
+	assert.NotNil(t, err)
+	assert.Nil(t, client)
+}
+
+func TestUpdateClient(t *testing.T) {
+	client, _ := NewClient("John Doe", "j0j.com")
+	err := client.Update("John Doe Update", "j0j.com")
+	assert.Nil(t, err)
+	assert.Equal(t, "John Doe Update", client.Name)
+	assert.Equal(t, "j0j.com", client.Email)
+}
+
+func TestUpdateClienttWithInvalidArgs(t *testing.T) {
+	client, _ := NewClient("John Doe", "j0j.com")
+	err := client.Update("", "j0j.com")
+	assert.Error(t, err, "name is required")
+}
