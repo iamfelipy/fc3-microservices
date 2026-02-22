2b404329018c46bf21e8913158bf58b855a4c151
felipy <felipy81br@gmail.com>
2026-02-22 16:14:25 +0000

    feat: create transaction usecase with unit test and gateway for wallet-core

---
+++ wallet-core/internal/usecase/create_transaction/create_transaction_test.go
@@ -0,0 +1,70 @@
+package createtransaction
+
+import (
+	"testing"
+
+	"github.com/iamfelipy/fc3-microservices/wallet-core/internal/entity"
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/mock"
+)
+
+type TransactionGatewayMock struct {
+	mock.Mock
+}
+
+func (m *TransactionGatewayMock) Create(transaction *entity.Transaction) error {
+	args := m.Called(transaction)
+	return args.Error(0)
+}
+
+type AccountGatewayMock struct {
+	mock.Mock
+}
+
+func (m *AccountGatewayMock) Save(account *entity.Account) error {
+	args := m.Called(account)
+	// se o on return que monta args, não tiver algo do tipo Error, retorna nil
+	return args.Error(0)
+}
+
+func (m *AccountGatewayMock) FindByID(id string) (*entity.Account, error) {
+	args := m.Called(id)
+	return args.Get(0).(*entity.Account), args.Error(1)
+}
+
+func TestCreateTransactionUseCase_Execute(t *testing.T) {
+	client1, _ := entity.NewClient("client1", "j@j.com")
+	account1 := entity.NewAccount(client1)
+	account1.Credit(1000)
+
+	client2, _ := entity.NewClient("client2", "j@j2.com")
+	account2 := entity.NewAccount(client2)
+	account2.Credit(1000)
+
+	mockAccount := &AccountGatewayMock{}
+	mockAccount.On("FindByID", account1.ID).Return(account1, nil)
+	mockAccount.On("FindByID", account2.ID).Return(account2, nil)
+
+	mockTransaction := &TransactionGatewayMock{}
+	mockTransaction.On("Create", mock.Anything).Return(nil)
+
+	inputDto := CreateTransactionInputDTO{
+		AccountIDFrom: account1.ID,
+		AccountIDTO:   account2.ID,
+		Amount:        100,
+	}
+
+	useCase := &CreateTransactionUseCase{
+		TransactionGateway: mockTransaction,
+		AccountGateway:     mockAccount,
+	}
+
+	output, err := useCase.Execute(inputDto)
+	assert.Nil(t, err)
+	assert.NotNil(t, output)
+	assert.NotEmpty(t, output.ID)
+	mockAccount.AssertExpectations(t)
+	mockTransaction.AssertExpectations(t)
+	mockAccount.AssertNumberOfCalls(t, "FindByID", 2)
+	mockTransaction.AssertNumberOfCalls(t, "Create", 1)
+}
