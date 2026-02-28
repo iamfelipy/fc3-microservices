9c3cc9f0142788090fa67f00d82e656413052e27
felipy <felipy81br@gmail.com>
2026-02-28 20:26:54 +0000

    feat: implement event dispatcher with tests

---
+++ utils-package/pkg/events/event_dispatcher.go
@@ -0,0 +1,66 @@
+package events
+
+import "errors"
+
+var ErrHandlerAlreadyRegistered = errors.New("handler already registered")
+
+type EventDispatcher struct {
+	handlers map[string][]EventHandlerInterface
+}
+
+func NewEventDispatcher() *EventDispatcher {
+	return &EventDispatcher{
+		handlers: make(map[string][]EventHandlerInterface),
+	}
+}
+
+func (ev *EventDispatcher) Dispatch(event EventInterface) error {
+	if handlers, ok := ev.handlers[event.GetName()]; ok {
+		for _, handler := range handlers {
+			handler.Handle(event)
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Register(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				// evitar sobrescrever ou efeitos duplicados
+				return ErrHandlerAlreadyRegistered
+			}
+		}
+	}
+
+	ed.handlers[eventName] = append(ed.handlers[eventName], handler)
+	return nil
+}
+
+func (ed *EventDispatcher) Has(eventName string, handler EventHandlerInterface) bool {
+	// nao da erro se eventName nao existir
+	if _, ok := ed.handlers[eventName]; ok {
+		for _, h := range ed.handlers[eventName] {
+			if h == handler {
+				return true
+			}
+		}
+	}
+	return false
+}
+
+func (ed *EventDispatcher) Remove(eventName string, handler EventHandlerInterface) error {
+	if _, ok := ed.handlers[eventName]; ok {
+		for i, h := range ed.handlers[eventName] {
+			if h == handler {
+				ed.handlers[eventName] = append(ed.handlers[eventName][:i], ed.handlers[eventName][i+1:]...)
+				return nil
+			}
+		}
+	}
+	return nil
+}
+
+func (ed *EventDispatcher) Clear() {
+	ed.handlers = make(map[string][]EventHandlerInterface)
+}
