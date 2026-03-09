5a67f3db7acde0b327c71b53c37006df775e34b5
felipy <felipy81br@gmail.com>
2026-03-09 04:12:48 -0300

    chore: restructure walletcore module

--- walletcore/internal/gateway/client.go
+++
@@ -1,8 +0,0 @@
-package gateway
-
-import "github.com/iamfelipy/fc3-microservices/walletcore/internal/entity"
-
-type ClientGateway interface {
-	Get(id string) (*entity.Client, error)
-	Save(client *entity.Client) error
-}
